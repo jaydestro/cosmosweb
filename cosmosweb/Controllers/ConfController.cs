069fc2988f47d989a170256f6cb85dc85cfc14fa
Mark Brown <mjbrown@microsoft.com>
2021-03-19 15:14:16 -0700

    Build custom agenda page and sessionize rest api.

--- cosmosweb/Controllers/ConfController.cs
+++ cosmosweb/Controllers/ConfController.cs
@@ -1,34 +1,55 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using cosmosweb.Services;
+using System.Threading.Tasks;
+using cosmosweb.Models;
+using System.Collections.Generic;
 
 namespace cosmosweb.Controllers
 {
     [AllowAnonymous]
     public class ConfController : Controller
     {
+
+        Sessionize sessionize = new Sessionize();
+
+        ConferenceSchedule ConferenceSchedule;
+
+        private async Task LoadSessions()
+        {
+            ConferenceSchedule = await sessionize.GetSessions();
+        }
+
         [Route("conf")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             return View("Index");
         }
 
         [Route("conf/agenda")]
-        public IActionResult Agenda()
+        public async Task<IActionResult> Agenda()
         {
             return View("Agenda");
         }
 
         [Route("conf/speakers")]
-        public IActionResult Speakers()
+        public async Task<IActionResult> Speakers()
         {
             return View("Speakers");
         }
 
         [Route("conf/countdown")]
-        public IActionResult Countdown()
+        public async Task<IActionResult> Countdown()
         {
             return View("CountDown");
+        }
+        [Route("conf/agenda2")]
+        public async Task<IActionResult> Agenda2()
+        {
+            if (ConferenceSchedule == null)
+                await LoadSessions();
+
+            return View("Agenda2", ConferenceSchedule);
         }
     }
 }
