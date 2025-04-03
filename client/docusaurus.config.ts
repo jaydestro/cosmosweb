4e4705cbd368a484d5a5359c0c0905764cb61479
Jay Gordon <2974195+jaydestro@users.noreply.github.com>
2025-04-03 12:34:06 -0400

    removed extra line

--- client/docusaurus.config.ts
+++ client/docusaurus.config.ts
@@ -86,7 +86,6 @@ const config: Config = {
     
   ],
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'Developer Home',
