0abda0450d464964d26c46a382063fea90fef494
Jay Gordon <2974195+jaydestro@users.noreply.github.com>
2026-01-14 18:27:23 -0500

    fixed gtag issues

--- client/src/clientModules/confMobileHashActive.ts
+++ client/src/clientModules/confMobileHashActive.ts
@@ -1,4 +1,4 @@
-function runWhenBrowser(fn: () => void): void {
+fix the function runWhenBrowser(fn: () => void): void {
   if (typeof window === "undefined" || typeof document === "undefined") return;
   fn();
 }
